bdb1ebc57a56ae049d18e9a086cfa95e51f38039
moto <855818+mthrok@users.noreply.github.com>
2020-06-28 15:10:50 +0000

    Enable ASAN

--- .circleci/unittest/linux/scripts/install.sh
+++ .circleci/unittest/linux/scripts/install.sh
@@ -20,4 +20,6 @@ printf "Installing PyTorch with %s\n" "${cudatoolkit}"
 conda install -y -c pytorch-nightly pytorch "${cudatoolkit}"
 
 printf "* Installing torchaudio\n"
-BUILD_SOX=1 python setup.py develop
+export LDFLAGS='-fsanitize=address'
+export BUILD_SOX=1
+python setup.py develop
--- .circleci/unittest/linux/scripts/run_test.sh
+++ .circleci/unittest/linux/scripts/run_test.sh
@@ -11,10 +11,13 @@ esac
 eval "$(./conda/bin/conda shell.bash hook)"
 conda activate ./env
 
-python -m torch.utils.collect_env
+apt install -y llvm
 export PATH="${PWD}/third_party/install/bin/:${PATH}"
-
+python -m torch.utils.collect_env
 if [ "${os}" == MacOSX ] ; then
+    export ASAN_OPTIONS=symbolize=1
+    export ASAN_SYMBOLIZER_PATH="$(which llvm-symbolizer)"
+    export LD_PRELOAD="${LD_PRELOAD} libasan.so"
     pytest -q -n auto --dist=loadscope --cov=torchaudio --junitxml=test-results/junit.xml --durations 20 test
 else
     pytest -v --cov=torchaudio --junitxml=test-results/junit.xml --durations 20 test

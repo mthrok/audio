723815039cb3b3257609385b8495b38ba34d9565
moto <855818+mthrok@users.noreply.github.com>
2020-07-28 15:04:36 +0000

    Add pybind11 binding to sox_io, sox_utils and sox_effects.

---
+++ torchaudio/csrc/bind.cpp
@@ -0,0 +1,24 @@
+#include <torch/extension.h>
+#include <torchaudio/csrc/sox_effects.h>
+#include <torchaudio/csrc/sox_io.h>
+#include <torchaudio/csrc/sox_utils.h>
+
+PYBIND11_MODULE(_torchaudio, m) {
+  py::class_<torchaudio::sox_utils::TensorSignal>(m, "TensorSignal")
+    .def(py::init<torch::Tensor, int64_t, bool>())
+    .def("get_tensor", &torchaudio::sox_utils::TensorSignal::getTensor)
+    .def("get_sample_rate", &torchaudio::sox_utils::TensorSignal::getSampleRate)
+    .def("get_channels_first", &torchaudio::sox_utils::TensorSignal::getChannelsFirst);
+
+  py::class_<torchaudio::sox_io::SignalInfo>(m, "SignalInfo")
+    .def("get_sample_rate", &torchaudio::sox_io::SignalInfo::getSampleRate)
+    .def("get_num_channels", &torchaudio::sox_io::SignalInfo::getNumChannels)
+    .def("get_num_frames", &torchaudio::sox_io::SignalInfo::getNumFrames);
+
+  m.def("get_info", &torchaudio::sox_io::get_info, "Gets information about an audio file.");
+  m.def("load_audio_file", &torchaudio::sox_io::load_audio_file, "Load audio file into Tensor.");
+  m.def("save_audio_file", &torchaudio::sox_io::save_audio_file, "Save Tensor as audio file.");
+
+  m.def("apply_effects_tensor", &torchaudio::sox_effects::apply_effects_tensor, "Apply sox effects to Tensor");
+  m.def("apply_effects_file", &torchaudio::sox_effects::apply_effects_file, "Apply sox effects to Tensor");
+}

90cd451d467390489e1f1e142b453408d489ee4c
moto <855818+mthrok@users.noreply.github.com>
2020-09-29 17:47:32 +0000

    Simplify main

--- main.cpp
+++ main.cpp
@@ -1,29 +1,22 @@
 #include <torch/script.h>
 
-#include <iostream>
-#include <vector>
-
 int main(int argc, char* argv[]) {
   if (argc !=4) {
     std::cerr << "Usage: " << argv[0] << " <JIT_OBJECT> <INPUT_FILE> <OUTPUT_FILE>" << std::endl;
     return -1;
   }
 
-  const std::string module_path = argv[1];
-  const std::string input_path = argv[2];
-  const std::string output_path = argv[3];
-
   torch::jit::script::Module module;
-  std::cout << "Loading module from: " << module_path << std::endl;
+  std::cout << "Loading module from: " << argv[0] << std::endl;
   try {
-    module = torch::jit::load(module_path);
+    module = torch::jit::load(argv[1]);
   } catch (const c10::Error &error) {
     std::cerr << "Failed to load the module:" << error.what() << std::endl;
     return -1;
   }
 
   std::cout << "Performing separation ..." << std::endl; 
-  std::vector<torch::jit::IValue> args{c10::IValue(input_path), c10::IValue(output_path)};
+  std::vector<torch::jit::IValue> args{c10::IValue(argv[2]), c10::IValue(argv[3])};
   module.forward(args);
   std::cout << "Done." << std::endl; 
 }

2bd16e8494e07df4e955b94c3084217696ba539e
moto <855818+mthrok@users.noreply.github.com>
2021-02-22 20:40:26 +0000

    Add sox patching framework

---
+++ debug/make_diff.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "${this_dir}"
+
+diff -ru base custom > ../third_party/sox/patch/sox.patch
+
+exit 0
